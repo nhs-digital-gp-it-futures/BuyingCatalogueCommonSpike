99cb25a324b892b506156c2a81447dbdea10c9f0
Callum <callum.bradbury1986@gmail.com>
2020-08-04 12:13:59 +0100

    Created Solution and Basic Serilog Projects

---
+++ src/NHSD.BuyingCatalogue.Common.Serilog/SerilogExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+namespace NHSD.BuyingCatalogue.Common.Serilog
+{
+    public static class SerilogExtensions
+    {
+        public static void ConfigureSerilog(this IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging(opts =>
+            {
+                opts.GetLevel = SerilogRequestLoggingOptions.GetLevel;
+            });
+        }
+    }
+}
