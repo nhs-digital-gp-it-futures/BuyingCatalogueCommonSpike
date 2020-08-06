c8cc1e6fe1eb7d27fb81fb32cc7c470e59520716
callum.bradbury@bjss.com <callum.bradbury@bjss.com>
2020-08-06 14:05:04 +0100

    replacing comment

--- src/NHSD.BuyingCatalogue.Common.Serilog/SerilogExtensions.cs
+++ src/NHSD.BuyingCatalogue.Common.Serilog/SerilogExtensions.cs
@@ -7,6 +7,7 @@ namespace NHSD.BuyingCatalogue.Common.Serilog
     {
         public static void ConfigureSerilog(this IApplicationBuilder app)
         {
+            // should up to 2.0.1-something.2
             app.UseSerilogRequestLogging(opts =>
             {
                 opts.GetLevel = SerilogRequestLoggingOptions.GetLevel;
