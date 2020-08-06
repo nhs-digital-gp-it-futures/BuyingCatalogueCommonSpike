1f17edde2d6ae6ad2d82a951f3ad3fc7761694ea
callum.bradbury@bjss.com <callum.bradbury@bjss.com>
2020-08-06 13:36:08 +0100

    removed comment as innocuous change

--- src/NHSD.BuyingCatalogue.Common.Serilog/SerilogExtensions.cs
+++ src/NHSD.BuyingCatalogue.Common.Serilog/SerilogExtensions.cs
@@ -5,7 +5,6 @@ namespace NHSD.BuyingCatalogue.Common.Serilog
 {
     public static class SerilogExtensions
     {
-        // innocuous comment to allow a commit
         public static void ConfigureSerilog(this IApplicationBuilder app)
         {
             app.UseSerilogRequestLogging(opts =>
